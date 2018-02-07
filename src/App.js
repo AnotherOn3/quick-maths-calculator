85e6dc65ec9083390f65ce1c54d3d9dc1a3df889
Gintaras Arstikaitis <ga@companyoung.com>
2018-02-07 11:05:07 +0100

    playing random sounds

--- src/App.js
+++ src/App.js
@@ -10,6 +10,25 @@ class App extends Component {
     result: '',
     quickMafs: ' quick mafs',
     shouldQuickMafsHide: true,
+    sounds: [
+      'https://cdn-resources.crowdcat.co/media/a027e3cf-bd0d-4183-83a1-081f6a47915a.mp3',
+      'https://cdn-resources.crowdcat.co/media/8432f01f-0bef-4928-8668-707dc228f378.mp3',
+      'https://cdn-resources.crowdcat.co/media/6c1901aa-8719-42e8-b00b-efb1ed0307b1.mp3',
+      'https://cdn-resources.crowdcat.co/media/6c1901aa-8719-42e8-b00b-efb1ed0307b1.mp3',
+      'https://cdn-resources.crowdcat.co/media/6ce2bb28-6067-4902-a8ec-b50882a43919.mp3',
+      'https://cdn-resources.crowdcat.co/media/927feb7b-9d17-443c-a12a-8258c103947f.mp3',
+      'https://cdn-resources.crowdcat.co/media/0961d6e1-81cd-48e2-8371-e56c5c9fc882.mp3',
+      'https://cdn-resources.crowdcat.co/media/4047b641-4f02-4b16-8336-1974f2f3c0d4.mp3',
+      'https://cdn-resources.crowdcat.co/media/0723121b-fd3e-47af-92d8-f72d74e43457.mp3',
+      'https://cdn-resources.crowdcat.co/media/d8f968c9-cfb1-465f-bb71-121025aa448d.mp3',
+      'https://cdn-resources.crowdcat.co/media/d1991e62-36fb-45c6-a4eb-41b44695d1c3.mp3',
+      'https://cdn-resources.crowdcat.co/media/afc19ca1-2d4d-4b86-935f-78ed13cb0bec.mp3',
+      'https://cdn-resources.crowdcat.co/media/b5afab28-676f-461f-b491-690a42a29d3b.mp3',
+    ],
+  };
+
+  getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
   add = () => {
@@ -18,9 +37,8 @@ class App extends Component {
         parseInt(this.state.firstInput) + parseInt(this.state.secondInput),
       shouldQuickMafsHide: false,
     });
-    const audio = new Audio(
-      'https://cdn-resources.crowdcat.co/media/a027e3cf-bd0d-4183-83a1-081f6a47915a.mp3',
-    );
+    const index = this.getRandomInt(1, this.state.sounds.length);
+    const audio = new Audio(this.state.sounds[index]);
     audio.play();
   };
 
