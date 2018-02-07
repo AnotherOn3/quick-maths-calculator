bc12e3346ddfbc48e2dbe3ba301a2a0004444a61
Gintaras Arstikaitis <ga@companyoung.com>
2018-02-07 10:33:16 +0100

    adding, minusing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    firstInput: '',
+    secondInput: '',
+    result: '',
+    quickMafs: ' quick mafs',
+    shouldQuickMafsHide: true,
+  };
+
+  add = () => {
+    this.setState({
+      result:
+        parseInt(this.state.firstInput) + parseInt(this.state.secondInput),
+      shouldQuickMafsHide: false,
+    });
+  };
+
+  minus = () => {
+    this.setState({
+      result:
+        parseInt(this.state.firstInput) - parseInt(this.state.secondInput),
+      shouldQuickMafsHide: false,
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          flexDirection: 'column',
+        }}
+      >
+        <h1>QUICK MATHS CALCULATOR</h1>
+        <div>
+          <input
+            onChange={event =>
+              this.setState({ firstInput: event.target.value })
+            }
+            type="number"
+          />
+          <input
+            onChange={event =>
+              this.setState({ secondInput: event.target.value })
+            }
+            type="number"
+          />
+          <br />
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-around',
+              alignItems: 'center',
+              marginTop: 30,
+            }}
+          >
+            <button onClick={this.add}>Add</button>
+            <button onClick={this.minus}>Minus</button>
+          </div>
+          <h2>
+            {this.state.result}
+            {this.state.shouldQuickMafsHide ? '' : this.state.quickMafs}
+          </h2>
+        </div>
       </div>
     );
   }
